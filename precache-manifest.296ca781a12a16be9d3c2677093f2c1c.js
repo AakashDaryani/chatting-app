9a2382b571ad6184c620e21da735e1dcfaf47e1d
Aakash Daryani <aakashdaryani50@gmail.com>
2023-05-15 12:53:38 +0530

    Fi

---
+++ precache-manifest.296ca781a12a16be9d3c2677093f2c1c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d38a29d9d10031a844eb",
+    "url": "/static/js/main.d38a29d9.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d309d147b8e5d12e6e39304e78fa3b3",
+    "url": "/index.html"
+  }
+];
